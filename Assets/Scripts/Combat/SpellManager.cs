4ea5b5be5bcacee2fc8c5428a026d19c4de99aa0
Joonyoung Kim <joony0123@gmail.com>
2019-02-24 21:32:40 -0500

    initial try

--- Assets/Scripts/Combat/SpellManager.cs
+++ Assets/Scripts/Combat/SpellManager.cs
@@ -3,7 +3,13 @@ using System.Collections;
 
 public class SpellManager : MonoBehaviour {
     ArrayList currentCast;
-    
+    private bool combo = false;
+    private bool canCast = true;
+    private float timer;
+    public float castingTime = 10f;
+    public Rigidbody projectile;
+    public float speed = 100f;
+
     // Use this for initialization
     void Start() {
         currentCast = new ArrayList();
@@ -11,31 +17,31 @@ public class SpellManager : MonoBehaviour {
 
     // Update is called once per frame
     void Update() {
-        /*
-        if (Input.GetKeyDown("p")) {
+        
+        if (Input.GetKeyDown("o")) {
             combo = true;
             GM.mgr_element.DisplayElement(Elements.Fire);
         }
 
-        if (Input.GetKeyDown("o") && combo) {
-            canCast = true;
-            combo = false;
-            GM.mgr_element.DisplayElement(Elements.Wind);
-        }
+        // if (Input.GetKeyDown("Fire1") && combo) {
+        //     canCast = true;
+        //     combo = false;
+        //     GM.mgr_element.DisplayElement(Elements.Wind);
+        // }
 
 
-        if (Input.GetButtonDown("Fire1") && canCast) {
+        if (Input.GetKeyDown("p") && canCast) {
             timer += Time.deltaTime;
-
+            
             if (timer >= castingTime) {
                 canCast = false;
                 timer = 0.0f;
             }
 
-            Rigidbody instantiatedProjectile = Instantiate(projectile, transform.position, transform.rotation) as Rigidbody;
+            Rigidbody instantiatedProjectile = Instantiate(projectile, new Vector3(transform.position.x, transform.position.y + 1, transform.position.z), transform.rotation) as Rigidbody;
 
             instantiatedProjectile.velocity = transform.TransformDirection(new Vector3(0, 0, speed));
         }
-        */
+        
     }
 }
