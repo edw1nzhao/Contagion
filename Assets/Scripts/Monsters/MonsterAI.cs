075a41686c86f26fe4c385fd2df14cbe8ffeefe2
Nathan Foote <39596241+Nanoparty@users.noreply.github.com>
2019-03-08 17:50:09 -0500

    MonsterAI

---
+++ Assets/Scripts/Monsters/MonsterAI.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class MonsterAI : MonoBehaviour
+{
+    NavMeshAgent nav;
+    public GameObject player;
+    // Start is called before the first frame update
+    void Start()
+    {
+        nav = GetComponent<NavMeshAgent>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 dest = GameObject.FindGameObjectWithTag("Player").transform.position;
+        var dist = Vector3.Distance(this.transform.position, dest);
+        if (dist < 10f) {
+            //Vector3 dest = player.transform.position;
+            //dest.y = this.transform.position.y;
+            nav.SetDestination(dest);
+            Debug.Log("player" + dest);
+        }
+       
+    }
+    private void FixedUpdate()
+    {
+        //Debug.Log(player.transform.position);
+    }
+}
