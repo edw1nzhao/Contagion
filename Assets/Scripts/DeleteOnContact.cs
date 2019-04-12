20459c5e48484b7f047244de57cc3fa2d8f8f61e
Nathan Foote <39596241+Nanoparty@users.noreply.github.com>
2019-04-12 17:48:09 -0400

    Potions Functionality/Monster Attack

---
+++ Assets/Scripts/DeleteOnContact.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DeleteOnContact : MonoBehaviour
+{
+
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        //Destroy(this.gameObject);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            Destroy(this.gameObject);
+            if(this.tag == "Health")
+                other.GetComponent<PlayerHealth>().addHealth(100);
+            if (this.tag == "Mana")
+                other.GetComponent<PlayerMana>().addMana(100);
+            if (this.tag == "EnemyProjectile")
+                other.GetComponent<PlayerHealth>().addHealth(-5);
+
+        }
+    }
+}
--- Assets/Scripts/Monsters/MonsterAI.cs
+++ Assets/Scripts/Monsters/MonsterAI.cs
@@ -7,15 +7,28 @@ public class MonsterAI : MonoBehaviour
 {
     NavMeshAgent nav;
     public GameObject player;
+    bool canAttack = true;
+    public int delay = 100;
+    public GameObject projectile;
+    int increment;
+
+
     // Start is called before the first frame update
+
     void Start()
     {
+       
         nav = GetComponent<NavMeshAgent>();
     }
 
     // Update is called once per frame
     void Update()
     {
+        if (!canAttack) {
+            increment++;
+            if (increment >= delay)
+                canAttack = true;
+        }
         Vector3 dest = GameObject.FindGameObjectWithTag("Player").transform.position;
         var dist = Vector3.Distance(this.transform.position, dest);
         if (dist < 10f) {
@@ -30,6 +43,16 @@ public class MonsterAI : MonoBehaviour
             if (nav.remainingDistance < 1.5)
             {
                 nav.Stop();
+                if (canAttack)
+                {
+                    GameObject bullet = Instantiate(projectile, transform.position, Quaternion.identity) as GameObject;
+                    var heading = transform.position - GameObject.FindGameObjectWithTag("Player").transform.position;
+                    var distance = heading.magnitude;
+                    var direction = heading / distance;
+                    bullet.GetComponent<Rigidbody>().AddForce(direction * -300);
+                    canAttack = false;
+                    increment = 0;
+                }
 
             }
             else {
