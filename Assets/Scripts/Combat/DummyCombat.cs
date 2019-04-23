abae33cedaf24bfc6052b5d0907b1ced02934fe8
Vy Huynh <vy@lawn-128-61-10-54.lawn.gatech.edu>
2019-04-22 20:15:32 -0400

    Changed Lighting + Reworked Dummyfor tutorial

---
+++ Assets/Scripts/Combat/DummyCombat.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DummyCombat : MonoBehaviour
+{
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.Equals("FireballCollider") || collision.gameObject.Equals("Fireball(Clone)"))
+        {
+            Destroy(gameObject);
+        }
+    }
+}
