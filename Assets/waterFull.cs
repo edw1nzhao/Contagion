da81423e483d6b2ebfbaa5d33ffaef5f0fb86ec1
Vy Huynh <vy@lawn-128-61-10-54.lawn.gatech.edu>
2019-04-25 17:54:48 -0400

    water full fix-no minions yet

--- Assets/waterFull.cs
+++ Assets/waterFull.cs
@@ -4,15 +4,21 @@ using UnityEngine;
 
 public class waterFull : MonoBehaviour
 {
+    private float level;
+    bool full = false;
     // Start is called before the first frame update
     void Start()
     {
-        
+        level = this.transform.position.y;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if((this.transform.position.y >= (level + (0.3f * 8)-.1f)) && !full) {
+            //move to level 3
+            Debug.Log("full");
+            full = true;
+        }
     }
 }
