3c45d2892ca08b51f7a4aed0992ba7d966837099
Necropuppy <nfooteaz@gmail.com>
2019-04-25 17:18:20 -0400

    fixed water fill

---
+++ Assets/dropletCollide.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class dropletCollide : MonoBehaviour
+{
+    public GameObject water;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void onTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player") {
+            Destroy(this.gameObject);
+            water.transform.position += new Vector3(0, 50, 0);
+        }
+    }
+}
